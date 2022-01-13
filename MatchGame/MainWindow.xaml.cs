46e7c125346a9d1e0867ba915692c11cbdec5ec3
robnimoa <ra.nietsch@posteo.de>
2022-01-13 16:19:15 +0100

    GUI and Random-Emojis

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -14,12 +14,31 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace MatchGame {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+
+            SetUpGame();
+        }
+
+        private void SetUpGame() {
+            List<string> animalEmoji = new List<string>() {
+                "🐵","🐵",
+                "🐷","🐷",
+                "🦄","🦄",
+                "🦍","🦍",
+                "🐔","🐔",
+                "🐻","🐻",
+                "🦑","🦑",
+                "🦩","🦩"
+            };
+            Random random = new Random();
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>()) {
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
         }
     }
 }
